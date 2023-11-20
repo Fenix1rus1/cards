f6d23e311eee3d8a80198d4849b3ee0d1b5d4ea6
fenix1rus1 <paivin_alexei@mail.ru>
2023-11-20 14:38:51 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PageViewSet } from '../src/components/PageViewSet';
 import { PageCreateSet } from '../src/components/PageCreateSet';
-
 import { PageSelectSet } from '../src/components/PageSelectSet';
 import { FormCreateSet } from '../src/components/FormCreateSet';
 import { FormCreateCard } from '../src/components/FormCreateCard';
-
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PageSelectSet/>} />
-        <Route path="/set/:id" element={<PageViewSet/>} />
-        <Route path="/admin" element={<PageCreateSet />} />
-          <Route path="createset" element={<FormCreateSet/>}/>
-          <Route path="createcard" element={<FormCreateCard/>}/>
-        <Route path="*" element={<PageSelectSet/>} />
+        <Route path="/" element={<PageSelectSet />} />
+        <Route path="/set/:id" element={<PageViewSet />} />
+        <Route path="/admin" element={<PageCreateSet />}>
+          <Route path="createset" element={<FormCreateSet />} />
+          <Route path="createcard" element={<FormCreateCard />} />
+        </Route>
+        <Route path="*" element={<PageSelectSet />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
-
-
--- src/components/FormCreateSet.js
+++ src/components/FormCreateSet.js
@@ -1,15 +1,82 @@
-export function FormCreateSet(){
-    return(
-        <form>
+import React, { useState } from 'react';
+import "./FormCreateSet.css"
+
+
+export function FormCreateSet() {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    // При изменении значения поля сбрасываем ошибку валидации
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: undefined,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validateForm(formData);
+    if (Object.keys(validationErrors).length === 0) {
+      // Ваш код для отправки данных
+      console.log('Данные отправлены:', formData);
+    } else {
+      // Обновляем состояние с ошибками валидации
+      setErrors(validationErrors);
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+  
+    // Пример валидации: название набора обязательно для заполнения
+    if (!data.name.trim()) {
+      errors.name = 'Введите название набора';
+    } else if (data.name.length < 5) {
+      errors.name = 'Название набора должно содержать минимум 5 символов';
+    }
+  
+    return errors;
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <div className='div_main'>
+            <div className='div_name'>
             <label>
-                название набора:
-                <input type="text" name="name"/>
+                Название набора:
+                <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                />
+                {errors.name && <div className="error">{errors.name}</div>}
             </label>
+            </div>
+            <div className='div_description'>
             <label>
-                описание:
-                <textarea name="discription"/>
+                Описание:
+                <textarea
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+                />
             </label>
-            <input type="submit" value="Создать сет"/>
-        </form>
-    )
+            
+            </div>
+            <div className='div_button'><input type="submit" value="Создать сет" /></div>
+              
+        </div>
+    </form>
+  );
 }
